f9ffa5e45549fc936cedf137b7c5fd25d128c0fe
Adam Beitz <61254754+abeitz@users.noreply.github.com>
2024-10-10 12:21:36 -0400

    Remove long_description from setup.py

--- setup.py
+++ setup.py
@@ -48,11 +48,7 @@ def main():
         keywords = "IDR",
         url = "https://github.com/nboley/idr",
 
-        long_description="""
-    The IDR (Irreproducible Discovery Rate) framework is a unified approach to measure the reproducibility of findings identified from replicate experiments and provide highly stable thresholds based on reproducibility. Unlike the usual scalar measures of reproducibility, the IDR approach creates a curve, which quantitatively assesses when the findings are no longer consistent across replicates. In layman's terms, the IDR method compares a pair of ranked lists of identifications (such as ChIP-seq peaks). These ranked lists should not be pre-thresholded i.e. they should provide identifications across the entire spectrum of high confidence/enrichment (signal) and low confidence/enrichment (noise). The IDR method then fits the bivariate rank distributions over the replicates in order to separate signal from noise based on a defined confidence of rank consistency and reproducibility of identifications i.e the IDR threshold.
-
-    The method was developed by Qunhua Li and Peter Bickel's group and is extensively used by the ENCODE and modENCODE projects and is part of their ChIP-seq guidelines and standards.
-    """,
+        
         classifiers=[
             "Programming Language :: Python :: 3",
             "Development Status :: 5 - Production/Stable",
