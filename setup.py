881fc55ed78728f6ecb68cdeb25baf6d0fa766e3
Adam Beitz <61254754+abeitz@users.noreply.github.com>
2024-10-10 11:38:57 -0400

    Update setup.py with new src folder

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ try:
     import numpy
     extensions = cythonize([
         Extension("idr.inv_cdf", 
-                  ["idr/inv_cdf.pyx", ],
+                  ["src/idr/inv_cdf.pyx", ],
                   include_dirs=[numpy.get_include()], compiler_directives={'language_level' : "3"}),
     ])
 except ImportError:
     import numpy
     extensions = [
         Extension("idr.inv_cdf", 
-                  ["idr/inv_cdf.c", ],
+                  ["src/idr/inv_cdf.c", ],
                   include_dirs=[numpy.get_include()]),
     ]
 
